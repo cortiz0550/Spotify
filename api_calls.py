12004abc5a5cc0d82fc917468c853fb1ece0bc88
cortiz <53792120+cortiz0550@users.noreply.github.com>
2023-05-17 15:53:10 -0700

    Add gitignore and update .env

--- api_calls.py
+++ api_calls.py
@@ -8,8 +8,8 @@ def configure():
     load_dotenv()
 
 configure()
-CLIENT_ID = os.getenv('client_id')
-CLIENT_SECRET = os.getenv('client_secret')
+CLIENT_ID = os.getenv('CLIENT_ID')
+CLIENT_SECRET = os.getenv('CLIENT_SECRET')
 
 AUTH_URL = 'https://accounts.spotify.com/api/token'
 BASE_URL = 'https://api.spotify.com/v1/'
