44b3624c9e9fc84516c04340c805eba1863662c1
cortiz <53792120+cortiz0550@users.noreply.github.com>
2023-05-17 15:45:29 -0700

    Testing authentication and first call

--- api_calls.py
+++ api_calls.py
@@ -1,3 +1,42 @@
 # This file will hold all of the api calls for Spotify that we may want.
 
-# This is a test.
+from dotenv import load_dotenv
+import os
+import requests
+
+def configure():
+    load_dotenv()
+
+configure()
+CLIENT_ID = os.getenv('client_id')
+CLIENT_SECRET = os.getenv('client_secret')
+
+AUTH_URL = 'https://accounts.spotify.com/api/token'
+BASE_URL = 'https://api.spotify.com/v1/'
+
+auth_response = requests.post(AUTH_URL, {
+    'grant_type': 'client_credentials',
+    'client_id': CLIENT_ID,
+    'client_secret': CLIENT_SECRET
+})
+
+ # convert the response to JSON
+auth_response_data = auth_response.json()
+
+# Save the access token.
+access_token = auth_response_data['access_token']
+
+""" Pull some data for an artist """
+headers = {
+    'Authorization': 'Bearer {token}'.format(token=access_token)
+}
+
+artist_id = '46gyXjRIvN1NL1eCB8GBxo'
+
+r = requests.get(BASE_URL + 'artists/' + artist_id, headers=headers)
+
+r = r.json()
+
+print(r)
+
+
